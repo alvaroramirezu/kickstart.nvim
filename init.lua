868467ffbaba256eb8168e132ae4c5b17bb45dbb
Alvaro Ramirez <alvaroramirezu@gmail.com>
2025-11-17 10:15:13 -0500

    Modd my configuration

--- init.lua
+++ init.lua
@@ -164,6 +164,7 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
+vim.opt.colorcolumn = '80'
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
